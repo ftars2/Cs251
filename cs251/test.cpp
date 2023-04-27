9d8ac55705a160e30617cde1b998b5bd4d8fe676
ftars2 <ftars2@uic.edu>
2023-04-26 22:12:03 -0500

    proj 6

--- cs251/test.cpp
+++ cs251/test.cpp
@@ -6,18 +6,8 @@ using namespace std;
 
 
 int main () {
-   queue<double>joyTime;
-   double yourNum = 0; 
-   joyTime.push(15.5);
-   joyTime.push(14.5);
-   joyTime.front();
-   joyTime.push(13.5);
-   joyTime.pop();
-   joyTime.push(12.5);
-   joyTime.pop();
-   joyTime.push(11.5);
-   joyTime.pop();
-   yourNum = joyTime.front() + joyTime.size();
-   cout << yourNum;
+int max=2;
+max*=2;
+cout<<max;
    return 0;
 }
